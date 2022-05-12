d4f104585cb78d9f345bdafdc93cc72f901114e3
dlghgus5656 <82634597+dlghgus5656@users.noreply.github.com>
2022-05-12 21:24:48 +0900

    CREATE REACT APP
    create react app
    module.css

--- react-movie/src/App.js
+++ react-movie/src/App.js
@@ -1,10 +1,11 @@
-// import logo from './logo.svg';
-// import './App.css';
+import Button from "./Button";
+import styles from "./App.module.css";
 
 function App() {
   return (
     <div>
-      <h1>Welcome Back!!</h1>
+      <h1 className={styles.title}>Welcome Back!!</h1>
+      <Button text={"Contunue"} />
     </div>
   );
 }
