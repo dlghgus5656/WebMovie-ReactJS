243864c3bec11b6ad3ca4c3a4d831262f1607e7e
dlghgus5656 <82634597+dlghgus5656@users.noreply.github.com>
2022-05-14 22:55:06 +0900

    useState
    useState

--- react-movie/src/App.js
+++ react-movie/src/App.js
@@ -1,10 +1,14 @@
 import Button from "./Button";
 import styles from "./App.module.css";
+import { useState } from "react";
 
 function App() {
+  const [counter, setValue] = useState(0);
+  const onClick = () => setValue((prev) => prev + 1);
   return (
     <div>
-      <h1 className={styles.title}>Welcome Back!!</h1>
+      <h1 className={styles.title}>{counter}</h1>
+      <button onClick={onClick}>click me</button>
       <Button text={"Contunue"} />
     </div>
   );
