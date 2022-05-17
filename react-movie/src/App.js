63951fc5a5b1893aa86c00f906bb6034fd0b26a7
dlghgus5656 <82634597+dlghgus5656@users.noreply.github.com>
2022-05-17 20:18:51 +0900

    useEffect
    useEffect hook

--- react-movie/src/App.js
+++ react-movie/src/App.js
@@ -5,7 +5,9 @@ import { useState, useEffect } from "react";
 // state를 변경할 때 모든 code들은 항상 다시 실행된다, 원치 않아도.
 function App() {
   const [counter, setValue] = useState(0);
+  const [keyword, setKeyword] = useState("");
   const onClick = () => setValue((prev) => prev + 1);
+  const onChange = (event) => setKeyword(event.target.value);
   console.log("항상 실행");
   const iRunOnlyOnce = () => {
     console.log("한번만 실행");
@@ -15,8 +17,23 @@ function App() {
   useEffect(() => {
     console.log("이것도 한번만 실행~");
   }, []);
+  useEffect(() => {
+    console.log("keyword가 변화할 때 실행", keyword);
+  }, [keyword]); // [] 안에 넣은 값이 바뀔때 마다 useEffect 내용을 실행
+  useEffect(() => {
+    console.log("counter가 변화할 때 실행");
+  }, [counter]);
+  useEffect(() => {
+    console.log("keyword와 counter 둘중 하나 라도  변화할 때 실행");
+  }, [keyword, counter]);
   return (
     <div>
+      <input
+        value={keyword}
+        onChange={onChange}
+        type="text"
+        placeholder="Search here"
+      />
       <h1 className={styles.title}>{counter}</h1>
       <button onClick={onClick}>click me</button>
       <Button text={"Contunue"} />
