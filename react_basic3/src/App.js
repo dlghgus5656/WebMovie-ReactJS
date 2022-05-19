702913e25825f67d39f0c9112ab9d3ff39c82163
dlghgus5656 <82634597+dlghgus5656@users.noreply.github.com>
2022-05-19 20:56:48 +0900

    To Do List part 2
    map() 함수
    -> 배열을 가지고 있을 때 각각의 element들을 바꿀 수 있게 해줌map() 은 ()에 함수를 넣을 수 있는데 배열의 모든 item에 대해 실행됨
    즉 배열에 6개의 item이 있다면 6번 함수가 실행됨
    그리고 그 함수로부터 내가 return한 값은 새로운 배열에 들어가게 함
    [‘a’, ‘b’, ‘c’, ‘d’, ‘e’, ‘f’].map(() => “:)”)
    -> [‘:)’, ‘:)’, ‘:)’, ‘:)’, ‘:)’ ‘:)’] 인 새 배열을 만들어줌
    다만 기존의 배열에 접근할 수 없게됨
    그러나 map은 함수의 첫 번째 argument로 현재의 item을 가지고 올 수 있음
    map(item) -> item이나 원하는 어떤 변수명을 넣으면 item자체를 리턴하는 것도 가능
    map((item) => item.toUpperCase())
    로 하면 item이 대문자로 바뀐 새로운 배열은 만들어줌
    
    리액트는 기본적으로 list에 있는 모든 item을 인식하기 때문에 key를 넣어 고유하게 만들어줘야함
    map의 첫 번째 argument는 값이고 두번째는 index 즉 숫자를 의미함
    그래서
    {toDos.map((item, index) => {item})}
    만들어줌
    즉,
    {{item},{item},{item}...}
    배열을 만들어 각자 고유의 key를 가지게 함

--- react_basic3/src/App.js
+++ react_basic3/src/App.js
@@ -27,6 +27,12 @@ function App() {
         />
         <button>Add To Do</button>
       </form>
+      <hr />
+      <ul>
+        {toDos.map((item, index) => (
+          <li key={index}>{item}</li>
+        ))}
+      </ul>
     </div>
   );
 }
@@ -49,4 +55,25 @@ function App() {
 // 그리고 byebye라고 적으면
 // currentArray에는 Hello 이미 있고 toDo는 byebye가 되는 것
 // 그리고 currentArray는 Hello와 byebye를 가지고 있는 배열이 됨
+
+// map() 함수
+// -> 배열을 가지고 있을 때 각각의 element들을 바꿀 수 있게 해줌map() 은 ()에 함수를 넣을 수 있는데 배열의 모든 item에 대해 실행됨
+// 즉 배열에 6개의 item이 있다면 6번 함수가 실행됨
+// 그리고 그 함수로부터 내가 return한 값은 새로운 배열에 들어가게 함
+// [‘a’, ‘b’, ‘c’, ‘d’, ‘e’, ‘f’].map(() => “:)”)
+// -> [‘:)’, ‘:)’, ‘:)’, ‘:)’, ‘:)’ ‘:)’] 인 새 배열을 만들어줌
+// 다만 기존의 배열에 접근할 수 없게됨
+// 그러나 map은 함수의 첫 번째 argument로 현재의 item을 가지고 올 수 있음
+// map(item) -> item이나 원하는 어떤 변수명을 넣으면 item자체를 리턴하는 것도 가능
+// map((item) => item.toUpperCase())
+// 로 하면 item이 대문자로 바뀐 새로운 배열은 만들어줌
+
+// 리액트는 기본적으로 list에 있는 모든 item을 인식하기 때문에 key를 넣어 고유하게 만들어줘야함
+// map의 첫 번째 argument는 값이고 두번째는 index 즉 숫자를 의미함
+// 그래서
+// {toDos.map((item, index) => {item})}
+// 만들어줌
+// 즉,
+// {{item},{item},{item}...}
+// 배열을 만들어 각자 고유의 key를 가지게 함
 export default App;
