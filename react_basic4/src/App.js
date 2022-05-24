035e7cefc2bafc8cec94a17f684f82fc72ccbc3e
dlghgus5656 <82634597+dlghgus5656@users.noreply.github.com>
2022-05-24 19:32:24 +0900

    Coin Tracker
    코인 API에는 이미 key가 있으므로 안 가져와도 됨
    
    이 경우 coin이라는 변수는 coins 배열 안에 있는 각각의 coin을 의미함
    그래서 각각의 coin은 https://api.coinpaprika.com/v1/tickers 처럼 생겼음
    그래서 coin.name도 coin.symbol도 가져올 수 있음
    
    처음에는 기본값으로 비어있는 배열을 넘겨주기 때문에 coin이 처음엔 0개
    기본값을 적어도 빈값으로 정해주지 않으면 에러가 남

--- react_basic4/src/App.js
+++ react_basic4/src/App.js
@@ -3,19 +3,56 @@ import { useEffect, useState } from "react";
 function App() {
   const [loading, setLoading] = useState(true);
   const [coins, setCoins] = useState([]);
+  const [myMoney, setMyMoney] = useState([]);
   useEffect(() => {
     //api 뒤 limit을 사용하면 받는 정보의 개수를 조정할 수 있다.
-    fetch("https://api.coinpaprika.com/v1/tickers?limit=20")
+    fetch("https://api.coinpaprika.com/v1/tickers?limit=4000")
       .then((response) => response.json())
       .then((json) => {
         setCoins(json);
         setLoading(false);
       });
   }, []);
+
+  function onChange(event) {
+    setMyMoney(event.target.value);
+  }
+
   return (
     <div>
-      <h1>코인입니다!</h1>
-      {loading ? <strong>Loading...</strong> : null}
+      <h1>코인입니다! {loading ? "" : `(${coins.length}개)`}</h1>
+      <input
+        onChange={onChange}
+        value={myMoney}
+        type="number"
+        placeholder="Please Write your USD"
+      />
+      {/* 삼항연산자 */}
+      {loading ? (
+        <strong>Loading...</strong>
+      ) : (
+        <select>
+          {/* {coins.map((coin) => (
+            <option>
+              {coin.name} ({coin.symbol}): {coin.quotes.USD.price} USD
+            </option>
+          ))} */}
+          {coins.map((coin) => (
+            <option key={coin.id}>
+              {coin.name} ({coin.symbol}):
+              {myMoney / coin.quotes.USD.price} {coin.symbol}
+            </option>
+          ))}
+        </select>
+      )}
+
+      <ul>
+        {coins.map((coin) => (
+          <li key={coin.id}>
+            {coin.name} ({coin.symbol}): {coin.quotes.USD.price} USD
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
